b79982fa9a81d9526c38dd6e7f8b58c75dd58aa6
yoselin <yoselingomm@gmail.com>
2022-10-13 17:29:59 -0400

    first commit4

---
+++ build/precache-manifest.8129b24c9e1809b40fea9424c1ab34d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0614b5716a3d1e35174e",
+    "url": "/static/css/main.d3104c31.chunk.css"
+  },
+  {
+    "revision": "0614b5716a3d1e35174e",
+    "url": "/static/js/main.0614b571.chunk.js"
+  },
+  {
+    "revision": "f25033e056dce94ebafe",
+    "url": "/static/js/1.f25033e0.chunk.js"
+  },
+  {
+    "revision": "01d483dcc7b3920c4934",
+    "url": "/static/css/2.ab4497ce.chunk.css"
+  },
+  {
+    "revision": "01d483dcc7b3920c4934",
+    "url": "/static/js/2.01d483dc.chunk.js"
+  },
+  {
+    "revision": "fd3cedb49ce3598d45ea",
+    "url": "/static/js/3.fd3cedb4.chunk.js"
+  },
+  {
+    "revision": "856ab5d1377e86503164",
+    "url": "/static/js/4.856ab5d1.chunk.js"
+  },
+  {
+    "revision": "1a68a16cb31bd09929f3",
+    "url": "/static/js/5.1a68a16c.chunk.js"
+  },
+  {
+    "revision": "6e6b9cdfab78cda2ae93",
+    "url": "/static/js/runtime~main.6e6b9cdf.js"
+  },
+  {
+    "revision": "3ac1cd50718e9759edeac291b61e6989",
+    "url": "/static/media/log.3ac1cd50.png"
+  },
+  {
+    "revision": "7017548989b4ebf71621cf636bfd80f0",
+    "url": "/static/media/llanten.70175489.jpeg"
+  },
+  {
+    "revision": "cfbfb9505d66962c679320d81b79779b",
+    "url": "/index.html"
+  }
+];
