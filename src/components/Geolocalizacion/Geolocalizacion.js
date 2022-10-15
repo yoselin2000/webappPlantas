569d8053f2a3f5ace489e17a607cb2e8d220aa68
yoselin2000 <109874011+yoselin2000@users.noreply.github.com>
2022-10-15 01:41:52 -0400

    Update Geolocalizacion.js

--- src/components/Geolocalizacion/Geolocalizacion.js
+++ src/components/Geolocalizacion/Geolocalizacion.js
@@ -1,121 +1,3 @@
-// import React, { useEffect, useState, useRef } from "react";
-
-// import { Map, TileLayer, Marker, Popup} from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-// import { IconLocation } from "./IconLocation";
-// import axios from "axios";
-// import '../../styles/geolocalizacion.css'
-// import useGeoLocation from "./useGeolocation";
-
-// const API = process.env.REACT_APP_API;
-
-// export const Geolocalizacion = () => {
-//   const [Loc, setLoc] = useState([]);
-//   const position = [-17.8965, -65.04534];
-//   const [busqueda, setBusqueda]= useState("");
-//   const mapRef = useRef();
-//   const location = useGeoLocation();
-//   //const position1 = [-17.89667, -65.03217]\
-//   useEffect(() => {
-//     getLoc();
-//   }, []);
-//   const getLoc = async () => {
-//     const result = await axios.get(`${API}/Localizacion`);
-//     console.log(result.data);
-//     setLoc(result.data);
-//   };
-
-
-
-//       const showMyLocation = () => {
-//         if (location.loaded && !location.error) {
-//           mapRef.current.leafletElement.flyTo(
-//             [location.coordinates.lat, location.coordinates.lng],
-//             { animate: true }
-//           );
-//         } else {
-//           alert(location.error.message);
-//         }
-//       };
-
-    
-//       const handleChange = (e) => {
-//         setBusqueda(e.target.value);
-//         filtrar(e.target.value);
-//       }
-      
-//       const filtrar = (terminoBusqueda)=>{
-    
-//         var resultadosBusqueda=Loc.filter((elemento)=>{
-//           if(elemento.nombre_planta.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-//           ){
-//             return elemento;
-//           }
-//         })
-//         setLoc(resultadosBusqueda);
-//       }
-    
-
-//   return (
-//     <div>
-//     <form className="form-inline" >
-//     <input className="form-control mr-sm-2" value={busqueda} placeholder = "SEARCH" onChange={handleChange}/>
-//     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//     </form>
-
-//     <Map
-//       className="map"
-//       center={position}
-//       zoom={10}
-//       style={{ height: 500, width: "100%" }}
-
-
-//     >
-//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-//       {Loc.map((Lo, index) => (
-//         <Marker
-//           position={[Lo.latitud, Lo.longitud]}
-//           icon={IconLocation}
-//           title={Lo.nombre_planta}
-//           // nombre={[planta.nombre_planta]}
-//         >
-//           <Popup>
-
-//             <img
-//               className="popup-img"
-//               // src={API + "/file/" + planta.imagen}
-//               src={Lo.imagen}
-//               alt={Lo.nombre_planta}
-//             />
-//             <h4></h4>
-//             {Lo.nombre_planta} <br /> {"Latitud: " + Lo.latitud} <br />{"Longitud: " + Lo.longitud}{" "}
-    
-//           </Popup>
-
-//         </Marker>
-
-//       ))}
-// {/* 
-//           <BaseLayer name="Satellite View">
-//             <TileLayer
-//                 url='https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
-//                 maxZoom= {20}
-//               />
-            
-//           </BaseLayer> */}
-
-//     </Map>
-//     </div>
-//   );
-// };
-
-// export default Geolocalizacion;
-
-
-
-
-// PRUEBA
-
 import React, { useEffect, useState, useRef, Link } from "react";
 
 import { Map, TileLayer, Marker, Popup, LayersControl} from "react-leaflet";
@@ -248,9 +130,6 @@ console.log(navigator.geolocation.getCurrentPosition(success, error, options))
         })}
       </LayersControl>
 
-      {/* <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-      {/* <TileLayer url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" /> */}
-
       {Loc.map((Lo, index, planta) => (
         <Marker
           position={[Lo.latitud, Lo.longitud]}
@@ -270,7 +149,7 @@ console.log(navigator.geolocation.getCurrentPosition(success, error, options))
             <h4></h4>
             {Lo.nombre_planta} <br /> {"Latitud: " + Lo.latitud} <br />{"Longitud: " + Lo.longitud}{" "} 
           <h1></h1>
-          <a href={`/planta_medicinal/${planta._id}`}>
+          <a href={`/About`}>
               <button className="btn btn-dark btn-sm ">CONOCER</button>
               </a>
 
